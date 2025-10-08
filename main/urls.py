f4cff53e1998a0056b406eb1e4db50c491d69e59
Hermes Trigs <allenroger451@gmail.com>
2025-10-08 15:06:31 +0100

    Updated: the CRUD system + styling

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns= [
     path('', views.index, name = 'index'),
+    path('home/', views.home, name = 'home'),
+    path('note/<int:pk>/', views.details, name ='details'),
+    path('add/', views.addNote, name= 'create'),
+
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from .forms import Noteform
+from .models import Note
 
 # Create your views here.
 def index(request):
     return render(request, 'main/index.html', {})
 
-#def home(request):
-   # return render(request, )
+def home(request):
+    notes = Note.objects.all()
+    return render(request, "main/home.html", {"notes": notes})
+
+def addNote(request):
+    form  = Noteform(request.POST  or None) 
+    if form.is_valid():
+        form.save()
+
+    return render(request,'main/createNote.html', {"form": form} )
+
+def details(request):
+    render(request, 'main/details.html', {})
